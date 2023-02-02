4947949f39bce85f18b771adeb09000e82e23847
shahank-attentive <124247506+shahank-attentive@users.noreply.github.com>
2023-02-02 14:27:14 +0530

    Add files via upload

---
+++ Week1/CurrencyConv.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def fun(base,conv,amount):
+    base_v = data_new['data'][base]['value']
+    conv_v = data_new['data'][conv]['value']
+    ans = amount * (conv_v / base_v)
+    return  ans
+
+response_API=requests.get('https://api.currencyapi.com/v3/latest?apikey=MrlfIugPXbWwLEW3kIfcihZVJXCIPn1E6gncTaOc')
+data=response_API.text   #converted to string
+data_new=json.loads(data) #converv to dict class
+# print(data_new)
+to_print=list(data_new['data'].keys())
+print('The list of currency symbols: \n',to_print)
+base=input("Enter the base currency: ")
+conv=input("Enter the converted currency: ")
+amount=int(input('Enter the amount to be converted: '))
+base_v=data_new['data'][base]['value']
+conv_v=data_new['data'][conv]['value']
+print(fun(base,conv,amount))
---
+++ Week1/RandPassGen.py
@@ -0,0 +1,18 @@
+import string
+import random
+dict1={1:'Contains Uppercase Character',2:'Contains Lowercase Character',3:'Contains Number',4:'Contains Special characters(only @, #, - and $)'}
+for key,value in dict1.items():
+    print(key,value)
+dict2={1:string.ascii_uppercase,2:string.ascii_lowercase,3:string.digits,4:'@#-$'}
+n=int(input("Enter number of rules you want in your password: "))
+RuleNo=list(dict1.keys())
+# print(RuleNo)
+randomList=random.sample(RuleNo,n)
+# print(randomList)
+finalStr=""
+for a in randomList:
+    finalStr+=dict2[a]
+length=int(input("Enter the length of password you want: "))
+ansList=random.sample(finalStr,length)
+ans="".join(ansList)
+print(ans)
