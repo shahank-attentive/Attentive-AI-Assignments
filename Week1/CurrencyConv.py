96a1a229e0eedbdb0e90f50bae30d2cc552b0d01
shahank-attentive <124247506+shahank-attentive@users.noreply.github.com>
2023-02-02 22:04:02 +0530

    Update CurrencyConv.py

--- Week1/CurrencyConv.py
+++ Week1/CurrencyConv.py
@@ -1,21 +1,19 @@
 import requests
 import json
-
-def fun(base,conv,amount):
-    base_v = data_new['data'][base]['value']
-    conv_v = data_new['data'][conv]['value']
-    ans = amount * (conv_v / base_v)
-    return  ans
-
+#API CALL
 resp=requests.get('https://api.currencyapi.com/v3/latest?apikey=MrlfIugPXbWwLEW3kIfcihZVJXCIPn1E6gncTaOc')
 data=resp.text   #converted to string
 data_new=json.loads(data) #converv to dict class
 # print(data_new)
 to_print=list(data_new['data'].keys())
 print('The list of currency symbols: \n',to_print)
+#INPUTS
 base=input("Enter the base currency: ")
 conv=input("Enter the converted currency: ")
 amount=int(input('Enter the amount to be converted: '))
+#FETCHING VALUES FROM API JSON(DICT CLASS) FILE
 base_v=data_new['data'][base]['value']
 conv_v=data_new['data'][conv]['value']
-print(fun(base,conv,amount))
+#CALCULATION
+ans = amount * (conv_v / base_v)
+print(ans)
