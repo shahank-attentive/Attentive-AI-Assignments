f7e3bc489dd5f9fcce1c803fac5da4d255c99036
shahank-attentive <124247506+shahank-attentive@users.noreply.github.com>
2023-02-08 15:20:06 +0530

    Added style, indentation and proper comments

--- Week1/CurrencyConv.py
+++ Week1/CurrencyConv.py
@@ -1,19 +1,25 @@
 import requests
 import json
-#API CALL
-resp=requests.get('https://api.currencyapi.com/v3/latest?apikey=MrlfIugPXbWwLEW3kIfcihZVJXCIPn1E6gncTaOc')
-data=resp.text   #converted to string
-data_new=json.loads(data) #converv to dict class
-# print(data_new)
-to_print=list(data_new['data'].keys())
-print('The list of currency symbols: \n',to_print)
-#INPUTS
-base=input("Enter the base currency: ")
-conv=input("Enter the converted currency: ")
-amount=int(input('Enter the amount to be converted: '))
-#FETCHING VALUES FROM API JSON(DICT CLASS) FILE
-base_v=data_new['data'][base]['value']
-conv_v=data_new['data'][conv]['value']
-#CALCULATION
+
+# API Call to fetch currency data(symbols and values) from currencyapi.com
+response_API = requests.get(
+    "https://api.currencyapi.com/v3/latest?apikey=MrlfIugPXbWwLEW3kIfcihZVJXCIPn1E6gncTaOc"
+)
+
+data_json = response_API.text  # Fetched data converted to string(json)
+data_dict = json.loads(data_json)  # Converts the string to dictionary format
+country_list = list(data_dict["data"].keys())
+print("The list of currency symbols: \n", country_list)
+
+# Taking inputs from user
+base = input("Enter the base currency: ")
+conv = input("Enter the converted currency: ")
+amount = int(input("Enter the amount to be converted: "))
+
+# Fetching specific values of country from dictionary data_dict
+base_v = data_dict["data"][base]["value"]
+conv_v = data_dict["data"][conv]["value"]
+
+# Calculation part
 ans = amount * (conv_v / base_v)
 print(ans)
